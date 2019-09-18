3ceaf7edf6eeadbf7026fe6f4b42bfd5482d8380
d <derekodai@gmail.com>
2019-09-17 22:51:15 -0400

    Updates

---
+++ component---src-pages-page-2-js-c374e0ec4d712bdb3c8d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{190:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(192),c=a(194),r=a(193);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-c374e0ec4d712bdb3c8d.js.map
---
+++ styles-9bbc437458dc36afd910.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{196:function(n,o,c){},199:function(n,o,c){},200:function(n,o,c){},204:function(n,o,c){}}]);
+//# sourceMappingURL=styles-9bbc437458dc36afd910.js.map
